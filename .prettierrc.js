adbef2f25042b570ba622917734ea67cebbfcd5b
gustavo.santos97 <gustavoms97@hotmail.com>
2020-09-18 20:08:14 -0300

    feat: inicia projeto com todas as configurações necessárias para trabalhar

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "consistent",
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
