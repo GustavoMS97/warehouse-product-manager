f28f2b5a004f97e0a08d799bba2fb3758b259947
gustavo.santos97 <gustavoms97@hotmail.com>
2020-10-02 11:22:27 -0300

    fix: corrige cagada do pato

--- src/infra/api/routes/find-product-by-warehouse-route.js
+++ src/infra/api/routes/find-product-by-warehouse-route.js
@@ -1,9 +1,9 @@
-exports.findProductByWareHouseRouteFactory = ({ findProductByWareHouseFactory } = {}) => {
+exports.findProductByWareHouseRouteFactory = ({ findProductByWareHouse } = {}) => {
   return {
     findProductByWareHouseRoute: async (req, res, next) => {
       try {
         const { warehouseId } = req.params;
-        const { products } = await findProductByWareHouseFactory({ warehouseId });
+        const { products } = await findProductByWareHouse({ warehouseId });
         return res.status(200).send({ products });
       } catch (findProductByWareHouseRouteError) {
         console.log(findProductByWareHouseRouteError);
