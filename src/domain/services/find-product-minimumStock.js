f812fbed0f39f76b9e49e679acdc6aa2c5053f1c
Alexandre <borges_2.0@hotmail.com>
2020-10-22 00:02:46 -0300

    Criação de service para trazer produtos abaixo do estoque minimo

---
+++ src/domain/services/find-product-minimumStock.js
@@ -0,0 +1,14 @@
+exports.findMinimumStockProductFactory = ({ Product } = {}) => {
+  return {
+    findMinimumStockProduct: async () => {
+      try {
+        const product = await Product.find();
+        const productMinimumStock = product.filter((product) => product.totalQuantity <= product.minimumInStock);
+        return { productMinimumStock };
+      } catch (findMinimumStockProductError) {
+        console.log(findMinimumStockProductError);
+        throw new Error('Não foi possivel obter os produtos abaixo do estoque minmo!');
+      }
+    },
+  };
+};
