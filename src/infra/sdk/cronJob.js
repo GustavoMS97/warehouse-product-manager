37c5f012b7717be8463a2e110f391b93d404751c
Alexandre <borges_2.0@hotmail.com>
2020-10-26 22:53:38 -0300

    inserido model de ShopingCart para metodo do Job

--- src/infra/sdk/cronJob.js
+++ src/infra/sdk/cronJob.js
@@ -3,13 +3,13 @@ const CronJob = require('cron').CronJob;
 const { processAbandonmentShoppingCartFactory } = require('./../../domain/use-cases/process-abandonment-shopping-cart');
 const { processAbandonmentShoppingCart } = processAbandonmentShoppingCartFactory();
 
-exports.CronJobFactory = ({ ENV } = {}) => {
+exports.CronJobFactory = ({ ENV, ShoppingCart } = {}) => {
   return {
     cronjob: ({ timeExecuteJob } = {}) => {
       const job = new CronJob(
         timeExecuteJob,
         function () {
-          processAbandonmentShoppingCart();
+          processAbandonmentShoppingCart(ENV, ShoppingCart);
         },
         null,
         true,
