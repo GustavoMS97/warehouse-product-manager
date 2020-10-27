1a4a789f58c5db8304a8bd6163c8153ce0535a9d
Gustavo Santos <gustavoms97@hotmail.com>
2020-10-26 22:29:09 -0300

    fix: corrige criação do carrinho

--- src/domain/services/create-shopping-cart.js
+++ src/domain/services/create-shopping-cart.js
@@ -2,7 +2,7 @@ exports.createShoppingCartFactory = ({ ShoppingCart } = {}) => {
   return {
     createShoppingCart: async ({ owner, product, quantity } = {}) => {
       try {
-        const shoppingCart = await ShoppingCart.create({ owner, product, quantity });
+        const shoppingCart = await ShoppingCart.create({ owner, productsQuantity: [{ product, quantity }] });
         return { shoppingCart };
       } catch (createShoppingCartError) {
         console.log(createShoppingCartError);
