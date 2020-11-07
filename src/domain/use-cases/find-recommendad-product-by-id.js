982a359c442d517de31491fdd76ee4a3cb743543
Alexandre <borges_2.0@hotmail.com>
2020-11-06 23:39:20 -0300

    Criação de use-case para produtos recomendados.

--- src/domain/use-cases/find-recommendad-product-by-id.js
+++ src/domain/use-cases/find-recommendad-product-by-id.js
@@ -1,9 +1,27 @@
-exports.findRecommendedProductByIdFactory = ({ Checkout } = {}) => {
+exports.findRecommendedProductByIdFactory = ({ Checkout, Product } = {}) => {
   return {
     findRecommendedProductById: async ({ id } = {}) => {
       try {
-        var checkout = await Checkout.find({ owner: id });
-        return { checkout };
+        const checkout = await Checkout.find({ owner: id }).populate('productMovements');
+        let allProductMovements = [];
+        for (let i = 0; i < checkout.length; i++) {
+          const checkoutItem = checkout[i];
+          const { productMovements } = checkoutItem;
+          allProductMovements.push(...productMovements);
+        }
+        let allProducts = [];
+        for (let i = 0; i < allProductMovements.length; i++) {
+          const productMovementItem = allProductMovements[i];
+          allProducts.push(productMovementItem.product);
+        }
+        const products = await Product.find({ _id: { $in: allProducts } });
+        let allCategory = [];
+        for (let i = 0; i < products.length; i++) {
+          const productsItem = products[i];
+          allCategory.push(productsItem.category);
+        }
+        const productsRecommended = await Product.find({ category: { $in: allCategory } });
+        return { productsRecommended };
       } catch (findRecommendedProductByIdError) {
         console.log(findRecommendedProductByIdError);
         throw new Error('Não foi possivel obter as recomendações!');
